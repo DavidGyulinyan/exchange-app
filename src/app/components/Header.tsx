2bdb9db972305ba5bc41b72df82431a5ef617382
davit <davidgyulinyan@gmail.com>
2025-01-10 13:19:11 +0400

    update 10.01.2025

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,42 +2,64 @@ import { Box, Typography } from "@mui/material";
 import CurrencyIcon from "../components/CurrencyIcon";
 
 export default function Header() {
-    return (
-        <Box
-            sx={{
-                width: "100%",
-                height: "200px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: {
-                    xs: "column",
-                    sm: "column",
-                    md: "row",
-                    lg: "row",
-                },
-                gap:"10px",
-                bgcolor: "#00214A",
-                color: "white",
-                fontFamily: "Inter, Arial, sans-serif"
-            }}
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "200px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: {
+          xs: "column",
+          sm: "column",
+          md: "row",
+          lg: "row",
+        },
+        gap: "10px",
+        bgcolor: "#00214A",
+        color: "white",
+        fontFamily: "Inter, Arial, sans-serif",
+      }}
+    >
+      <CurrencyIcon
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: {
+              xs: "25px",
+              sm: "30px",
+              md: "35px",
+              lg: "35px",
+            },
+          }}
         >
-            <CurrencyIcon onClick={function (): void {
-                throw new Error("Function not implemented.");
-            } } />
-
-            <Typography
-                sx={{
-                    fontSize: {
-                        xs: "25px",
-                        sm: "30px",
-                        md: "35px",
-                        lg: "35px",
-                    },
-                }}
-            >
-                Best Exchange Rates
-            </Typography>
-        </Box>
-    )
+          RateSnap
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: {
+              xs: "10px",
+              sm: "10px",
+              md: "15px",
+              lg: "15px",
+            },
+          }}
+        >
+          Best Exchange Rates
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
