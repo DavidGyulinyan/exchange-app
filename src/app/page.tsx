98b47b6d882450e73853d6cbbe163b3e2bcf2102
davit <davidgyulinyan@gmail.com>
2025-01-13 15:28:59 +0400

    first 'to amount' is 4th currency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
             const initialFromCurrency =
               storedHistory[0]?.from || Object.keys(data.conversion_rates)[0];
             const initialToCurrency =
-              storedHistory[0]?.to || Object.keys(data.conversion_rates)[1];
+              storedHistory[0]?.to || Object.keys(data.conversion_rates)[4];
 
             setFromCurrency(initialFromCurrency);
             setToCurrency(initialToCurrency);
